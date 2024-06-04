06bc6c0af2122f01f2655dbbad93eb23eefbd1d2
Kayque Oliveira <58827916+oliveirakay@users.noreply.github.com>
2024-06-04 03:05:58 +0000

    Revisando sintaxe

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 1;
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+
+        scanner.close();
     }
 }
